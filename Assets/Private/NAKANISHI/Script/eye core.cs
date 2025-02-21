47813f62eb1841f0a39d965db708c1473bb4c286
hw24a097 <>
2025-02-22 02:35:47 +0900

    インターバルを削除

--- Assets/Private/NAKANISHI/Script/eye core.cs
+++ Assets/Private/NAKANISHI/Script/eye core.cs
@@ -7,7 +7,6 @@ using UnityEngine.XR;
 
 public class eyecore : MonoBehaviour
 {
-    bool intervaleye = false; //目の処理のインターバル
     bool eyeopen = false; //eyeが開いているか
     bool Cinwall = false; //カーソルがwallの中にいるか
     bool Cineye = false; //カーソルがeyeの中にいるか
@@ -17,7 +16,7 @@ public class eyecore : MonoBehaviour
         Vector3 mousePosition = Input.mousePosition; //カーソルの位置を取得
         Vector3 target = Camera.main.ScreenToWorldPoint(mousePosition);//ワールド座標に変換
 
-        if (eyeopen == false && Cinwall == false && intervaleye == false) //eyeopen&Cinwallがfalseなら
+        if (eyeopen == false && Cinwall == false) //eyeopen&Cinwallがfalseなら
         {
             if (Input.GetMouseButtonDown(0)) //左クリックを取得
             {
@@ -25,35 +24,24 @@ public class eyecore : MonoBehaviour
             }
 
         }
-        if (eyeopen == true && intervaleye == false) //eyeopneとCineyeがtrueなら
+        if (eyeopen == true && Cineye == true) //eyeopneとCineyeがtrueなら
         {
             if (Input.GetMouseButtonDown(0)) //左クリックを取得
             {
                 Invoke(nameof(DestroyEye), 0f);
             }
         }
-        if (intervaleye == true)
-        {
-            Invoke(nameof(IntervalEye), 1f);
-        }
-
 
     }
-    void IntervalEye()
-    {
-        intervaleye = false;
-        CancelInvoke(nameof(IntervalEye));
-    }
+
     void SummonEye()
     {
-        intervaleye = true;
         eyeopen = true; //trueにする
         Debug.Log("eyeopen");
         CancelInvoke(nameof(SummonEye));
     }
     void DestroyEye()
     {
-        intervaleye = true;
         eyeopen = false; //falseにする
         Debug.Log("eyeclose");
         CancelInvoke(nameof(DestroyEye));
